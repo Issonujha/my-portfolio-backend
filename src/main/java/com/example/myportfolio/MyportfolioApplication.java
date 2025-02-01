e5874776f46e923d6d1ebbf539b009a4eada82b8
Sonu Jha <sjha9563@gmail.com>
2025-02-01 18:45:46 +0530

    Updated code

--- src/main/java/com/example/myportfolio/MyportfolioApplication.java
+++ src/main/java/com/example/myportfolio/MyportfolioApplication.java
@@ -9,22 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MyportfolioApplication {
 
 	public static void main(String[] args) {
-		// Load the .env file
-        Dotenv dotenv = Dotenv.load();
-
-        // Set system environment variables
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("EMAIL", dotenv.get("EMAIL"));
-        System.setProperty("EMAIL_PASSWORD", dotenv.get("EMAIL_PASSWORD"));
-        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
 		SpringApplication.run(MyportfolioApplication.class, args);
 	}
 	
