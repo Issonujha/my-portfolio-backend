7bfdbda41adbc7774be9321b8ad4fd3a8a96c975
Sonu Jha <sjha9563@gmail.com>
2025-02-02 22:22:03 +0530

    Updated For APIS

--- src/main/java/com/example/myportfolio/controller/HomeController.java
+++ src/main/java/com/example/myportfolio/controller/HomeController.java
@@ -1,16 +1,14 @@
 package com.example.myportfolio.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class HomeController {
 	
-	@GetMapping
+	@GetMapping("/")
 	public String home() {
-		return "Welcome to my api section";
+		return "index";
 	}
 
 }
